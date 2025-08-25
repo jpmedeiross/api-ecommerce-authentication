1c2336517d7d47fcaa38726c3cfb50abb0cedd30
João Pedro Medeiros <jpmdrsdev@gmail.com>
2025-08-25 03:31:08 -0300

    chore: connection with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT') || '5432', 10),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
